7ed891f552452be52530904e869b37745476d19a
GamingRex4274 <grex4274@gmail.com>
2023-12-05 23:10:33 -0400

    Last adjustments to window.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 
 int main()
 {
+    // Create window.
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Bogo Gift Sort", sf::Style::Close);
+
+    // Set window icon.
+    const int icon_dim = 32;
+    sf::Image icon;
+    icon.loadFromFile("src\\Sprites\\gift.png");
+    window.setIcon(icon_dim, icon_dim, icon.getPixelsPtr());
+
+    // Window settings.
     window.setFramerateLimit(60);
     window.setKeyRepeatEnabled(false);
 
+    // Start game loop.
     Game game(window);
     while (window.isOpen())
         game.run();
